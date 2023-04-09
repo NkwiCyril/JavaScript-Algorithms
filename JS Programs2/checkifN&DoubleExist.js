5bea7f9e7991ddaf43d3629f29c7efadfceab879
CYRIX-CCS <akinimbomnkwi@gmail.com>
2023-04-09 20:58:45 +0100

    checking element double exists

---
+++ JS Programs2/checkifN&DoubleExist.js
@@ -0,0 +1,8 @@
+// check if N and its double exist in an array
+
+let numArray = [1,2,3,4];
+for (let i = 0; i < numArray.length; i++){
+    for(let j = numArray.length; j > 0; j++){
+        
+    }
+}
