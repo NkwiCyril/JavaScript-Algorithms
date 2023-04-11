c5cd905be830e38afce252123cb5e3c93074fe23
CYRIX-CCS <akinimbomnkwi@gmail.com>
2023-04-11 11:12:46 +0100

    added js

--- JS Programs2/my_jquery_functions.js
+++ JS Programs2/my_jquery_functions.js
@@ -1,3 +1,10 @@
+if($('.first') != $('.second')){
+    alert("First p element not equal to second p element.")
+}
+else {
+    alert("They are equal!")
+}
 $('.first').css('color','blue')
 $('.second').css('color','red')
 $('.third').css('color','green')
+
